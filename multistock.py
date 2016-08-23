39e9a18112a9b245a1dba6ef9ca0f8564dcae82b
root <root@localhost.localdomain>
2016-08-22 17:55:38 -0700

    added global statistics along with rolling mean, std to create bollinger bands

--- multistock.py
+++ multistock.py
@@ -61,6 +61,8 @@ def join_and_slice():
 	#print df1.ix['2016-08-01' : '2016-08-22', [str(sys.argv[1]), str(sys.argv[2])]]
 		
 	plot(normalize(df2))
+	global_stats(df2)
+	
 
 def normalize(dframe):
 	#normalize stock prices using the first row of the dataframe
@@ -78,6 +80,65 @@ def plot(dframe):
 	axis.set_xlabel("Date")
 	axis.set_ylabel("Adj Close Price")
 	plt.show()
+
+def global_stats(dframe):
+	#at least 33 global stats
+		
+	print "Mean:\n", dframe.mean()
+	print "Median:\n", dframe.median()
+	print "Std. Deviation(measure of deviation from central value; high value means price has varied a lot over time):\n", dframe.std()
+
+	#rolling mean: mean from a range(aka a window of prices) -- lags behind actual price aka rolling -- simple moving average -- look at where the price crosses through the rolling average
+	#hypothesis -- rolling mean may be a good representation of sort of the true underlying price of a stock, and that significant deviations from that, eventually result in a return to the mean 
+	#might indicate a good buying low opportunity
+	#the challenge is to know when is that deviation significant enough to that you should pay attention
+	#high deviation may also indicate a sell signal/opportunity 
+
+	#assuming we're using a rolling mean and tracking the price, and we are looking for an opportunity to find when the prices diverged significantly far from the rolling mean that might be an opportunity
+	#for a buy or sell signal -- how can we decide that we're far enough away from the mean that we should consider buying or selling? -- which statistic might we use to discover this?
+		#use rolling standard deviation to discover an opportunity to sell or buy
+	#how can we know if a deviation from the rolling mean is significant enough to warrant a trading signal? we need some way of measuring that
+	#John Bollinger -- Bollinger bands (trademark) -- Bollinger observed that we ought to take a look at the recent volatility of the stock
+		# if it's very volatile we might discard movements above and below the mean 
+		# if it's not very volatile a similarly sized movement may be we should pay attention to
+	#Bollinger bands (trademark) -- add a band 2 std deviations above rolling mean, and a band 2 std devations below rolling average
+		# the theory is that when you see excursions up to 2 sigma (aka 2 std deviations) away from the mean, you should pay attention
+		# in particular, if prices drop below or above 2 sigma and then back through it, that is potentially a buy or sell signal 
+			# because the hypothesis is that we've gone quite far from the simple moving average and we are now moving back towards it
+	
+		
+	for symbol in dframe[0:]:
+		axis = dframe[symbol].plot(title=symbol, label=symbol)
+		rolling_mean = get_rolling_mean(dframe[symbol], windowSize=20)	
+		rolling_std = get_rolling_std(dframe[symbol], windowSize=20)
+		upper_band, lower_band = get_bollinger_bands(rolling_mean, rolling_std)
+	
+		rolling_mean.plot(label="Rolling Mean", ax=axis)
+		rolling_std.plot(label="Rolling STD", ax=axis)
+		upper_band.plot(label="upper band", ax=axis)
+		lower_band.plot(label="lower band", ax=axis)	
+
+		axis.set_xlabel("Date")
+		axis.set_ylabel("Adj Close Price")
+		axis.legend(loc='upper left')
+		plt.show()
+		
+
+	#Daily Returns: are one of the most important stats used in financial analysis
+
+
+def get_rolling_mean(df_values, windowSize):
+	return pd.rolling_mean(df_values, windowSize)	
+
+def get_rolling_std(df_values, windowSize):
+	return pd.rolling_std(df_values, windowSize)
+
+def get_bollinger_bands(rolling_mean, rolling_std):
+	upper_band = rolling_mean + rolling_std * 2
+	lower_band = rolling_mean - rolling_std * 2
+	return upper_band, lower_band
+
+
 
 if __name__ == "__main__":
 	if len(sys.argv) > 3:
